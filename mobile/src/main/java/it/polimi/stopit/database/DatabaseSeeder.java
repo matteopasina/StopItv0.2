86a45b30b7562d3ddca08020be5f8b7da9594a43
Alessio Rossotti <alessiorossotti@gmail.com>
2015-12-13 16:02:30 +0100

    fixes

--- mobile/src/main/java/it/polimi/stopit/database/DatabaseSeeder.java
+++ mobile/src/main/java/it/polimi/stopit/database/DatabaseSeeder.java
@@ -20,6 +20,10 @@ public class DatabaseSeeder {
         db.addAchievement(new Achievement(3,"One Week Stop","Not smoke for one week",600,"void",false));
         db.addAchievement(new Achievement(4,"Two Weeks Stop","Not smoke for two weeks",800,"void",false));
         db.addAchievement(new Achievement(5,"One Month Stop","Not smoke for one month",1000,"void",false));
+        db.addAchievement(new Achievement(6,"Beginner","Do an alternative activity",100,"void",false));
+        db.addAchievement(new Achievement(6,"10 Activities","Do 10 alternative activities",200,"void",false));
+        db.addAchievement(new Achievement(7,"20 Activities","Do 20 alternative activities",400,"void",false));
+        db.addAchievement(new Achievement(8,"50 Activities","Do 50 alternative activities",600,"void",false));
 
     }
 }
